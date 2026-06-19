30a381433ff53bf3ec3ba2ea3c239f368282fa9c
HISSANE Omar Adam <hissaneomaradam@users.noreply.github.com>
2026-06-19 20:57:24 +0100

    deployment bug fix

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
 import { PortfolioOS } from "../components/PortfolioOS/PortfolioOS";
-import { Analytics } from '@vercel/analytics/next';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 export default function App() {
   return (
     <>
