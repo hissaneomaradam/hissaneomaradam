8539141eb14327ee5584e6cd2bd823ed239d7095
HISSANE Omar Adam <hissaneomaradam@users.noreply.github.com>
2026-06-19 20:54:46 +0100

    some deployment stuff

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,12 @@
 import { PortfolioOS } from "../components/PortfolioOS/PortfolioOS";
-
+import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 export default function App() {
-  return <PortfolioOS />;
+  return (
+    <>
+      <PortfolioOS />
+      <Analytics />
+      <SpeedInsights />
+    </>
+  );
 }
